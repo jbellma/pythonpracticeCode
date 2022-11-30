5d26ef38537124c32e70881d1ef2285bfc0db11d
Jose Bello <bellojosema11@gmail.com>
2022-11-30 12:16:12 +0000

    Band name generator

---
+++ 100-days-of-code/day1_project_band_generator.py
@@ -0,0 +1,5 @@
+# Generating a band name
+print("Welcome to the Band Name Generator,")
+city = input("What's the name of the city you grew in? ")
+pet = input("What's your pet's name? ")
+print(f"Your band name could be {city.title()} {pet.title()}")
