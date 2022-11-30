7bf85faf56e3da69b0cb69ee20b543c5b1358c0b
Jose Bello <bellojosema11@gmail.com>
2022-11-30 09:34:44 +0000

    Index positions start at 0, not 1

--- python-crash-course/basics/bicycles.py
+++ python-crash-course/basics/bicycles.py
@@ -1,6 +1,8 @@
 bicycles = ['trek', 'canondale', 'redline', 'specialized']
+
 print(bicycles)
-
 print(bicycles[0])
-
 print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])  #Asking for the last element in the list
