d90ddf5180752d132cf6b22ee0a78da34c6438bc
Jose Bello <bellojosema11@gmail.com>
2022-11-30 09:39:37 +0000

    Using individual values form a list

--- python-crash-course/basics/bicycles.py
+++ python-crash-course/basics/bicycles.py
@@ -1,8 +1,18 @@
 bicycles = ['trek', 'canondale', 'redline', 'specialized']
-
+#Accessing elements in a list
 print(bicycles)
 print(bicycles[0])
+
+# Using string methods on list elements
 print(bicycles[0].title())
+
+# AIndex positions start at 0, not 1
 print(bicycles[1])
 print(bicycles[3])
-print(bicycles[-1])  #Asking for the last element in the list
+
+# Asking for the last element in the list
+print(bicycles[-1])
+
+# Using individual values from a list
+message = f"My first bycicle was a {bicycles[0].title()}."
+print(message)
