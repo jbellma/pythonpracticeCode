421d2f032d64af682097850a7462a835a4836312
Jose Bello <bellojosema11@gmail.com>
2022-11-30 09:59:27 +0000

    Inserting elements into a list

--- python-crash-course/basics/motorcycles.py
+++ python-crash-course/basics/motorcycles.py
@@ -15,3 +15,8 @@ motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
 print(motorcycles)
+
+# Inserting elements into a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
