a2e0c9e4dba1f979f18fcf11140ffd063d9372a8
Jose Bello <bellojosema11@gmail.com>
2022-12-01 08:09:53 +0000

    Adding digits in a two digit number

---
+++ 100-days-of-code-bootcamp/day2_exercise1_data_types.py
@@ -0,0 +1,9 @@
+# Adding digits in a 2 digit number
+
+two_digit_number = input("Type a two digit number: ")
+
+digit_one = int(two_digit_number[0])
+digit_two = int(two_digit_number[1])
+
+sum = digit_one + digit_two
+print(sum)
