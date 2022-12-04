1702cea8c651ef5f0030d5ca9f6a16417bbb2217
Jose Bello <bellojosema11@gmail.com>
2022-12-04 08:37:26 +0000

    working with a value that is being REMOVED from a list

--- python-crash-course/basics/motorcycles.py
+++ python-crash-course/basics/motorcycles.py
@@ -60,5 +60,8 @@ print(f"The first motorcycle I owned was a {first_owned.title()}.")
 motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
 print(motorcycles)
 
-motorcycles.remove('ducati')
+too_expensive = 'ducati'
+print(too_expensive)
+motorcycles.remove(too_expensive)
 print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
