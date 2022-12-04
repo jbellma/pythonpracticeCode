1c86f708e0c418333d6c3d2c2025cdcf28a6ddc4
Jose Bello <bellojosema11@gmail.com>
2022-12-04 08:51:06 +0000

    Sorting a list temporarily with the sorted() function

--- python-crash-course/basics/cars.py
+++ python-crash-course/basics/cars.py
@@ -7,3 +7,15 @@ print(cars)
 cars = ['bmw', 'audi', 'toyota', 'subaru',]
 cars.sort(reverse=True)
 print(cars)
+
+# sorting a list temporarily with the sorted() function
+cars = ['bmw', 'audi', 'toyota', 'subaru',]
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
