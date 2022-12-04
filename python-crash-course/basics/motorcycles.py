49d8cca0d82031bab6e4858e3ef967a436927b22
Jose Bello <bellojosema11@gmail.com>
2022-12-04 08:12:50 +0000

    pop method to print a statement about the last motorcycle we owned

--- python-crash-course/basics/motorcycles.py
+++ python-crash-course/basics/motorcycles.py
@@ -41,3 +41,9 @@ print(motorcycles)
 popped_motorcycle = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+last_owned = motorcycles.pop()
+print(last_owned)
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
