4fb4009486a2368b551134fff0af3374f69fb0dd
Jose Bello <bellojosema11@gmail.com>
2022-12-06 08:28:34 +0000

    updating comments in code

--- python-crash-course/basics/first_numbers.py
+++ python-crash-course/basics/first_numbers.py
@@ -1,8 +1,11 @@
+# Using th range function. Python stops counting at the second value provided.
 for value in range(1, 5):
     print(value)
 
+# Using the range function to include the second value provided (5).
 for value in range(1, 6):
     print(value)
 
+# Using the list() function to convert the results of range() into a list.
 numbers = list(range(1, 6))
 print(numbers)
