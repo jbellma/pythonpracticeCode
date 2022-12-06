aab735204629602ff483507d97bdd3325053a89f
Jose Bello <bellojosema11@gmail.com>
2022-12-06 07:51:12 +0000

    Adding a second message to each item

--- python-crash-course/basics/magicians.py
+++ python-crash-course/basics/magicians.py
@@ -1,3 +1,4 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next tree, {magician.title()}.\n")
