545b9a91bdd642ac70091828a660054b88e3626b
Jose Bello <bellojosema11@gmail.com>
2022-12-06 08:20:01 +0000

    Making numerical lists

--- python-crash-course/basics/magicians.py
+++ python-crash-course/basics/magicians.py
@@ -2,3 +2,5 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
     print(f"I can't wait to see your next tree, {magician.title()}.\n")
+
+print(f"Thank you, everyone. That was a great magic show!")
