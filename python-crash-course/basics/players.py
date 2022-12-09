12d71bce4f432af3119cd5f93fc26748676b78e3
Jose Bello <bellojosema11@gmail.com>
2022-12-09 10:37:41 +0000

    Slice with no second index

--- python-crash-course/basics/players.py
+++ python-crash-course/basics/players.py
@@ -1,4 +1,5 @@
 players =['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[0:3])
-print(players[1:4])
-print(players[:4])
+print(players[0:3]) # Slice
+print(players[1:4]) # Slice
+print(players[:4])  # Slice were we omit the firts index
+print(players[2:])  # Slice were we omit the second index
