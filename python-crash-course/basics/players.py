436fef62bbe98d3966088ac18cd4b07d65273934
Jose Bello <bellojosema11@gmail.com>
2022-12-09 10:39:57 +0000

    Slice with the last three players on the roster

--- python-crash-course/basics/players.py
+++ python-crash-course/basics/players.py
@@ -3,3 +3,4 @@ print(players[0:3]) # Slice
 print(players[1:4]) # Slice
 print(players[:4])  # Slice were we omit the firts index
 print(players[2:])  # Slice were we omit the second index
+print(players[-3:]) # Slice with the last three players on the roster
