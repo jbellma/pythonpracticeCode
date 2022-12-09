ea8a0f8d7fabb418f2589341792fe6151c4f2a71
Jose Bello <bellojosema11@gmail.com>
2022-12-09 15:58:10 +0000

    Checking that a list is not empty

--- python-crash-course/basics/toppings_three.py
+++ python-crash-course/basics/toppings_three.py
@@ -1,9 +1,12 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+requested_toppings = []
 
-for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers':
-        print("Sorry, we are out of green peppers right now.")
-    else:
-        print(f"Adding {requested_topping}.")
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            print("Sorry, we are out of green peppers right now.")
+        else:
+            print(f"Adding {requested_topping}.")
+else:
+    print("Are you sure you want a plain pizza?")
 
 print("\nFinished making your pizza!")
