bd150512322ac1634185342bcb7045205fd38b13
Jose Bello <bellojosema11@gmail.com>
2022-12-09 12:16:17 +0000

    Reassigning a new value to a variable that represents a tuple

--- python-crash-course/basics/dimensions.py
+++ python-crash-course/basics/dimensions.py
@@ -4,3 +4,19 @@
 dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
+
+# Looping thrpugh all values in a tuples
+
+for dimension in dimensions:
+    print(dimension)
+
+# Writing over a tuple
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
