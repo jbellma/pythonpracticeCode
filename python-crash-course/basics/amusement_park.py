2a1d8b6bc6fcfbff9d8ca5f7a42725e8f044690c
Jose Bello <bellojosema11@gmail.com>
2022-12-09 15:09:30 +0000

    If-elif-else statements concised code version

--- python-crash-course/basics/amusement_park.py
+++ python-crash-course/basics/amusement_park.py
@@ -1,8 +1,10 @@
-age = 12
+age = 19
 
 if age < 4:
-    print("Your admission cost is $0.")
+    price = 0
 elif age < 18:
-    print("Your admission cost is $25.")
+    price = 25
 else:
-    print("Your admission cost is $40.")
+    price = 40
+
+print(f"Your admission cost is ${price}.")
