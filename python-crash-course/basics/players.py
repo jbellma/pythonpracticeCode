868ac54d7a499bd55eb57309ffb1a0b5dd8f013e
Jose Bello <bellojosema11@gmail.com>
2022-12-09 10:45:25 +0000

    Looping through a slice

--- python-crash-course/basics/players.py
+++ python-crash-course/basics/players.py
@@ -1,6 +1,14 @@
+# Slicing a list
+
 players =['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3]) # Slice
 print(players[1:4]) # Slice
 print(players[:4])  # Slice were we omit the firts index
 print(players[2:])  # Slice were we omit the second index
 print(players[-3:]) # Slice with the last three players on the roster
+
+# Looping through a slice
+
+print("Here are the first three players on my team: ")
+for player in players[:3]:
+    print(player.title())
