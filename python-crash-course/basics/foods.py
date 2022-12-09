d81c21b230ca64cc7f7585ff8033259c3af31a31
Jose Bello <bellojosema11@gmail.com>
2022-12-09 11:09:01 +0000

    Proving that we actually have two separate lists

--- python-crash-course/basics/foods.py
+++ python-crash-course/basics/foods.py
@@ -8,3 +8,12 @@ print(my_foods)
 
 print("\nMy friend's favorite foods are: ")
 print(friend_foods)
+
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
+print("\nMy favorite foods are: ")
+print(my_foods)
+
+print("\nMy friend's favorite foods are: ")
+print(friend_foods)
