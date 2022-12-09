2048441c76d0b9ec5beb7773140cc9c948e810e0
Jose Bello <bellojosema11@gmail.com>
2022-12-09 15:40:39 +0000

    Checking for special items update

--- python-crash-course/basics/toppings_three.py
+++ python-crash-course/basics/toppings_three.py
@@ -1,6 +1,9 @@
 requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
 for requested_topping in requested_toppings:
-    print(f"Adding {requested_topping}.")
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
 
-print("\nFinished making your pizza")
+print("\nFinished making your pizza!")
