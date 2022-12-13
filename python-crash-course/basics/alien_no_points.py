4b18ab7ee41bcc8a4eb36df5760230726baefc98
Jose Bello <bellojosema11@gmail.com>
2022-12-13 13:52:27 +0000

    Getting default value if key does not exist

--- python-crash-course/basics/alien_no_points.py
+++ python-crash-course/basics/alien_no_points.py
@@ -1,1 +1,6 @@
 # Using get() to access values
+alien_0 = {'color': 'green', 'speed': 'slow'}
+# print(alien_0['points'])
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
