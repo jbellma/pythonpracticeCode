3e09214170925e41dd223b0d5db61059aa841fbc
Jose Bello <bellojosema11@gmail.com>
2022-12-13 15:06:39 +0000

    Accessing the value associated with desired key

--- python-crash-course/basics/favorite_languages.py
+++ python-crash-course/basics/favorite_languages.py
@@ -1,5 +1,4 @@
 # Storing results from a poll in a dictionary
-
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
@@ -11,11 +10,19 @@ language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
 
 # Looping through all key-value pairs in a dictionary
-
 for name, language in favorite_languages.items():
     print(f"\n{name.title()}'s favorite language is {language.title()}.")
 
 # Looping through all the keys in a dictionary
-
 for name in favorite_languages.keys():
-    print(name.title())
+    print(f"\n{name.title()}")
+
+
+# Accessing the value associated with the desired key.
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"\tHi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
