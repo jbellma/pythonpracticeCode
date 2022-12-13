4f425a597d153acc6f8d25259bd29ce13a362e96
Jose Bello <bellojosema11@gmail.com>
2022-12-13 13:43:43 +0000

    Dictionary with similar objects

---
+++ python-crash-course/basics/favorite_languages.py
@@ -0,0 +1,11 @@
+# Storing results from a poll in a dictionary
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarahs favorite language is {language}.")
