2a04dc004f5208c92a11b2a6f4e46e0847bbacde
Jose Bello <bellojosema11@gmail.com>
2022-12-13 12:47:55 +0000

    Dictionaries

--- python-crash-course/basics/alien.py
+++ python-crash-course/basics/alien.py
@@ -1,3 +1,26 @@
+# Accessing values in a dictionary
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just have earned {new_points} points!")
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+# Adding key-value pairs to the dictionary
+alien_0['x_postion'] = 0
+alien_0['y_postion'] = 25
+
+print(alien_0)
+
+# Starting with an empty dictionary
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+# Modifying values in a dictionary
