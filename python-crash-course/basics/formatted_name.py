00b579e4060742aaba9f12b3106cd40f02d48bbc
Jose Bello <bellojosema11@gmail.com>
2022-12-14 18:02:05 +0000

    Making an argument optional

--- python-crash-course/basics/formatted_name.py
+++ python-crash-course/basics/formatted_name.py
@@ -6,3 +6,12 @@ def get_formatted_name(first_name, second_name):
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+
+# Making and argument optional
+def get_formatted_name(first_name, middle_name, last_name):
+    """Return a full name, neatly formatted"""
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('john', 'lee', 'hooker')
+print(musician)
