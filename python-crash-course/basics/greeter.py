83381f684e706902d204a1a27a3f326ef540f74a
Jose Bello <bellojosema11@gmail.com>
2022-12-14 11:59:44 +0000

    Building a multiline string

--- python-crash-course/basics/greeter.py
+++ python-crash-course/basics/greeter.py
@@ -1,2 +1,8 @@
 name = input("Please enter your name: ")
 print(f"\nHello, {name}!")
+
+prompt = "If you share your name, we can personalize the messages you see."
+prompt += "\nWhat is your fisrt name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
