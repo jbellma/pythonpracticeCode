64ffda4085a85f95e8b049c2f186f99a16b6a089
Jose Bello <bellojosema11@gmail.com>
2022-12-14 17:29:23 +0000

    Default Values

--- python-crash-course/basics/pets_two.py
+++ python-crash-course/basics/pets_two.py
@@ -11,3 +11,14 @@ describe_pet('dog', 'willie')
 # Keyword arguments
 describe_pet(animal_type='hamster', pet_name= 'harry')
 describe_pet(pet_name= 'harry', animal_type= 'hamster')
+
+# Default values
+def describe_pet(pet_name, animal_type = 'dog'): # Order of parameters in the function definition had to be changed (Python still interprets this as a positional argument)
+    """Display information about pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(pet_name = 'willie')
+describe_pet('willie')
+describe_pet(pet_name = 'harry', animal_type= 'hamster')
