c405c254e54cc74d051409c6aaf9f4a29ecd7f69
Jose Bello <bellojosema11@gmail.com>
2022-12-15 16:49:07 +0000

    Creating multiple instances

--- python-crash-course/basics/dog.py
+++ python-crash-course/basics/dog.py
@@ -18,6 +18,14 @@ class Dog:
 my_dog = Dog('Willie', 6)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
-
 my_dog.sit()
 my_dog.roll_over()
+
+print("\n--- ---\n")
+
+# Creating multiple instances.
+
+your_dog = Dog('Lucy', 3)
+print(f"Your dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
