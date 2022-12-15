97999a1ab3b458e8f72e9e513c33f37d25e17c5d
Jose Bello <bellojosema11@gmail.com>
2022-12-15 17:31:37 +0000

    Modifying an attribute's value directly

--- python-crash-course/basics/car.py
+++ python-crash-course/basics/car.py
@@ -20,3 +20,8 @@ class Car:
 my_new_car = Car('audi', 'a4', '2004')
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
+
+# Modifying an attribute's value directly
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
