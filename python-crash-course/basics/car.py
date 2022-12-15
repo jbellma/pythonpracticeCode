9a1427d2ea7d18530147134ac49e6ab8360cde62
Jose Bello <bellojosema11@gmail.com>
2022-12-15 18:02:26 +0000

    Incrementing an attribute's value through a method

--- python-crash-course/basics/car.py
+++ python-crash-course/basics/car.py
@@ -27,23 +27,40 @@ class Car:
         else:
             print("You can't roll back an odometer!")
 
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
 my_new_car = Car('audi', 'a4', '2004')
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 
 # Modifying an attribute's value directly
+print("\n--- ---\n")
 
-# my_new_car.odometer_reading = 23
-# my_new_car.read_odometer()
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
 
 # Modifying an attribute’s value through a method.
+print("\n--- ---\n")
 
 my_new_car.update_odometer(30)
 my_new_car.read_odometer()
-
 
 my_new_car.update_odometer(35)
 my_new_car.read_odometer()
 
 my_new_car.update_odometer(20)
 my_new_car.read_odometer()
+
+# Incrementing an attribute’s value through a method.
+print("\n--- ---\n")
+
+my_used_car = Car('subaru', 'outback', '2019')
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
