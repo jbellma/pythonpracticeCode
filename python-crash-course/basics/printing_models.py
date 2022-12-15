463f30eeb6d1257ba2bfbe53edc70146aa6a0203
Jose Bello <bellojosema11@gmail.com>
2022-12-15 11:48:29 +0000

    Modifying a list in a function

--- python-crash-course/basics/printing_models.py
+++ python-crash-course/basics/printing_models.py
@@ -1,6 +1,6 @@
 # Modifying a list in a function.
 
-# Start with some designs that need to be printed.
+# Start with some designs that need to be printed. Code WITHOUT functions.
 
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
@@ -14,4 +14,34 @@ while unprinted_designs:
 print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
-    
+
+
+print("\n--- Next ---\n")
+
+
+# Start with some designs that need to be printed. Code WITH functions.
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until none are left.
+    Move each design to completed
+    """
+
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+# Display all completed models
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed"""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
