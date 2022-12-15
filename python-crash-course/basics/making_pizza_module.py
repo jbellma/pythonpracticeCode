e9e7ca6136423dce75951c576b23c7afa2e0de7b
Jose Bello <bellojosema11@gmail.com>
2022-12-15 13:37:22 +0000

    Importing an entire module

---
+++ python-crash-course/basics/making_pizza_module.py
@@ -0,0 +1,6 @@
+#Importing modules.
+
+import pizza_module
+
+pizza_module.make_pizza(16, 'pepperoni')
+pizza_module.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
--- python-crash-course/basics/user_profile.py
+++ python-crash-course/basics/user_profile.py
@@ -1,4 +1,4 @@
-# Mixing arbitray keyword arguments.
+# Mixing arbitrary keyword arguments.
 def build_profile(first, last, **user_info):
     """Build a dictionary containing everything we know about a user."""
     user_info['first_name'] = first
