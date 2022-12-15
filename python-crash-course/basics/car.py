f8e94cbb24ffab982a722200a03380a95104a7a6
Jose Bello <bellojosema11@gmail.com>
2022-12-15 17:51:37 +0000

    Logic added to prevent people from rolling back the odometer reading

--- python-crash-course/basics/car.py
+++ python-crash-course/basics/car.py
@@ -18,8 +18,14 @@ class Car:
         print(f"This car has {self.odometer_reading} miles on it.")
 
     def update_odometer(self, mileage):
-        """Set the odometer reading to the given value"""
-        self.odometer_reading = mileage
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
 
 my_new_car = Car('audi', 'a4', '2004')
 print(my_new_car.get_descriptive_name())
@@ -27,10 +33,17 @@ my_new_car.read_odometer()
 
 # Modifying an attribute's value directly
 
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
 
 # Modifying an attribute’s value through a method.
 
-my_new_car.update_odometer(45)
+my_new_car.update_odometer(30)
+my_new_car.read_odometer()
+
+
+my_new_car.update_odometer(35)
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(20)
 my_new_car.read_odometer()
