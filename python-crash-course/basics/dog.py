54589156d73f55742343cee0876786801ad2071f
Jose Bello <bellojosema11@gmail.com>
2022-12-15 16:43:10 +0000

    Accessing attributes and calling methods

--- python-crash-course/basics/dog.py
+++ python-crash-course/basics/dog.py
@@ -7,8 +7,17 @@ class Dog:
 
     def sit(self):
         """Simulate a dog sitting in response to a command."""
-        print(f"{self.name} is now sitting")
+        print(f"{self.name} is now sitting.")
 
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
+
+# Creating an instance representing a specific dog.
+
+my_dog = Dog('Willie', 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+
+my_dog.sit()
+my_dog.roll_over()
