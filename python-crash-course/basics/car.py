63f83cd4900a155968045bd5da9bd2748f7b7f40
Jose Bello <bellojosema11@gmail.com>
2022-12-15 17:17:53 +0000

    Setting a default value for an attribute

--- python-crash-course/basics/car.py
+++ python-crash-course/basics/car.py
@@ -6,12 +6,17 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Retun a neatly formatted descriptive name."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
 
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
 my_new_car = Car('audi', 'a4', '2004')
 print(my_new_car.get_descriptive_name())
-
+my_new_car.read_odometer()
