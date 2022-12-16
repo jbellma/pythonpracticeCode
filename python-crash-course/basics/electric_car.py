d58f1a409e051409ff106f30546d4eb64eeda3b1
Jose Bello <bellojosema11@gmail.com>
2022-12-16 10:23:55 +0000

    Instances as attributes

--- python-crash-course/basics/electric_car.py
+++ python-crash-course/basics/electric_car.py
@@ -37,7 +37,15 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
-        self.battery_size =40
+        self.battery = Battery()
+
+
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=90):
+        """Initialize the battery size."""
+        self.battery_size = battery_size
 
     def describe_battery(self):
         """Print a statement describing the battery size."""
@@ -45,4 +53,4 @@ class ElectricCar(Car):
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
+my_leaf.battery.describe_battery()
