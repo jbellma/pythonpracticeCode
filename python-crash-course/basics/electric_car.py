c90339c93055db8934beae1de5e9ad6518359338
Jose Bello <bellojosema11@gmail.com>
2022-12-16 09:55:27 +0000

    Defining attributes and methods for the child class

--- python-crash-course/basics/electric_car.py
+++ python-crash-course/basics/electric_car.py
@@ -37,6 +37,12 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
+        self.battery_size =40
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()
