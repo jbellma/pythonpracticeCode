4cf58b50c3903430896e46f030419550d0fa883b
Jose Bello <bellojosema11@gmail.com>
2022-12-16 12:51:45 +0000

    Accessing a file's lines

--- python-crash-course/basics/file_reader.py
+++ python-crash-course/basics/file_reader.py
@@ -2,5 +2,7 @@ from pathlib import Path
 
 path = Path('pi_digits.txt')
 contents = path.read_text()
-contents = contents.rstrip()
-print(contents)
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
