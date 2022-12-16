a54c5f240878ebb0e9c3720812750d925553293d
Jose Bello <bellojosema11@gmail.com>
2022-12-16 10:55:53 +0000

    Storing multiple classes in a module

--- python-crash-course/basics/car_module.py
+++ python-crash-course/basics/car_module.py
@@ -1,7 +1,8 @@
-"""A class that can be used to represent a car."""
+"""A set of classes used to represent gas and electric cars."""
 
 
 class Car:
+
     """A simple attempt to represent a car."""
 
     def __init__(self, make, model, year):
@@ -33,3 +34,33 @@ class Car:
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=65):
+        """Initialize the battery size."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
