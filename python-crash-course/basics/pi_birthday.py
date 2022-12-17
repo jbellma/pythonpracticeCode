deb795980fd295a3e06bfb38578bd7468b7b59b6
Jose Bello <bellojosema11@gmail.com>
2022-12-17 12:58:14 +0000

    Birthday contained in pi update?

--- python-crash-course/basics/pi_birthday.py
+++ python-crash-course/basics/pi_birthday.py
@@ -12,4 +12,4 @@ birthday = input("Enter your birthday, in the fomr mmddyy: ")
 if birthday in pi_string:
     print("Your birthday appears in the first million digits of pi.")
 else:
-    print("Your birthday does not appear in the first million digits of pi.")
+    print("Your birthday does not appear in the first million digits of pi!")
