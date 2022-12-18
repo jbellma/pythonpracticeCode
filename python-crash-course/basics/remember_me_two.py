6fb322784369aeae0a809ae68f8f54d8817a01f7
Jose Bello <bellojosema11@gmail.com>
2022-12-18 09:47:10 +0000

    Refactoring remember_me_two.py

--- python-crash-course/basics/remember_me_two.py
+++ python-crash-course/basics/remember_me_two.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 
+def greet_user():
+    """Greet the user by name."""
 path = Path('username.json')
 if path.exists():
     contents = path.read_text()
@@ -11,3 +13,5 @@ else:
     contents = json.dumps(username)
     path.write_text(contents)
     print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
