9d22043bf1999f3af2f0a991ff479831ffc23d6b
Jose Bello <bellojosema11@gmail.com>
2022-12-21 12:30:33 +0000

    Responding to a failed test

--- python-crash-course/basics/test_name_function.py
+++ python-crash-course/basics/test_name_function.py
@@ -2,5 +2,5 @@ from name_function_two import get_formatted_name
 
 def test_first_last_name():
     """Do names like 'Janis Joplin' work?"""
-    formatted_name = get_formatted_name('janis', 'joplin', 'arthur')
-    assert formatted_name == 'Janis Joplin Arthur'
+    formatted_name = get_formatted_name('janis', 'joplin')
+    assert formatted_name == 'Janis Joplin'
