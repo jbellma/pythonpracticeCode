d5a19c4507062be61011f053e52a785b50213a82
Jose Bello <bellojosema11@gmail.com>
2023-04-05 13:09:07 +0100

    Project Part Nine

---
+++ python-and-django-bootcamp/javascript_level_one/project_part_nine.js
@@ -0,0 +1,20 @@
+var firstName = prompt("Hello and Welcome. Please enter your Fisrt Name:");
+var secondName = prompt("Please enter your Second Name :");
+var age = prompt("How old are you?");
+var height = prompt("How tall are you?")
+var petName = prompt("Please enter your Pet's Name :");
+alert("Thank you so much for the information.");
+
+var lastChar = petName[petName.length -1];
+
+if(firstName[0] === "J"){
+  if (secondName[0] === "J"){
+    if (age > 20 && age < 30){
+      if(height >= 170){
+          if (lastChar === "y"){
+            console.log("Welcome Secret Agent.")
+          }
+      }
+    }
+  }
+}
